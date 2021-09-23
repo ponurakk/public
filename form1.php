049614d8a0f512e10172ae7680c71f2a889a512e
ponurakk <91278893+ponurakk@users.noreply.github.com>
2021-09-23 18:32:05 +0200

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<?php $testvar = "text" ?>
+Welcome <?php echo $_POST["userName"]; ?><br>
+Your password is: <?php echo $_POST["userPass"]; ?><br>
+Test var: <?php echo $testvar?>
+
+</body>
+</html> 
